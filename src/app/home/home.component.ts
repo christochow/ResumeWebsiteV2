a01886a3fc79c40aca06353ae348900243ad0bd9
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2019-09-10 16:27:18 -0400

    updated styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title:string;
+  title: string;
   constructor() {
     this.title = 'Welcome to my website';
   }
